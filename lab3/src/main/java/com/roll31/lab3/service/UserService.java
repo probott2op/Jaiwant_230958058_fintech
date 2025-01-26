821c6a218b110e8518f6d79307c88a56d49fa960
probott <137513308+probott2op@users.noreply.github.com>
2025-01-26 08:40:54 +0530

    Made little changes to ERD, customer detail added 1. added customer detail to excel 2. made little changes to ERD, making full name derieved 3. The DDl and DML script was altered and now it works perfect and the whole database seems logical now

--- lab3/src/main/java/com/roll31/lab3/service/UserService.java
+++ lab3/src/main/java/com/roll31/lab3/service/UserService.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class UserService {
-@Autowired
-private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
